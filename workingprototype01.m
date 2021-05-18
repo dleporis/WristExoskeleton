494a51c63e0cc7073f414baefa51a04ada2b9038
AndreiVoica <45489065+AndreiVoica@users.noreply.github.com>
2021-05-18 13:35:45 +0200

    Update workingprototype01.m
    Cleaned the code. Motor limitations added

--- workingprototype01.m
+++ workingprototype01.m
@@ -5,22 +5,12 @@ addpath('./Resources/')                                                    % dir
 
 shimmer = ShimmerHandleClass('8');                                     % Define shimmer as a ShimmerHandle Class instance with comPort1
 SensorMacros = SetEnabledSensorsMacrosClass;                               % assign user friendly macros for setenabledsensors
-
-%firsttime = true;
-
-%NO_SAMPLES_IN_PLOT = 500;                                                  % Number of samples that will be displayed in the plot at any one time
+                                                 
 DELAY_PERIOD = 0.2;                                                        % A delay period of time in seconds between data read operations
-%numSamples = 0;
 Motor1 = Epos4(1,0);
 Motor1.EnableNode;
-Motor1.SetOperationMode( OperationModes.ProfileVelocityMode );
-%Motor1.SetOperationMode( OperationModes.ProfilePositionMode );
+Motor1.SetOperationMode(OperationModes.ProfileVelocityMode);
 Motor1.ClearErrorState;
-
-%Motor1.SetOperationMode(OperationModes.CurrentMode);
-%Motor1.MotionWithCurrent(100)
-
-%%
 
 if shimmer.connect                                                      % TRUE if the shimmer connects
     % Define settings for shimmer
@@ -36,6 +26,8 @@ if shimmer.connect                                                      % TRUE i
     signalNameArray = shimmer.getenabledsignalnames;
 end
 
+n = 50;
+m = 50;
 bool = 1;
 plotData = [];
 while bool == 1
@@ -46,14 +38,11 @@ while bool == 1
     plotData = [plotData; newData];
     %plot(plotData(:,[x]));
     a = plotData(:,[x]);
-    %number = int64(a);
-    loop = 1;
+    i = Motor1.ActualPosition;
     if a > 0
-        disp('up')
         plotData = [];
         Motor1.MotionInVelocity(100);
     else
-        disp('down')
         plotData = [];
         Motor1.MotionInVelocity(-100);
     end
@@ -63,37 +52,21 @@ while bool == 1
     end
     if a > 2
         plotData = [];
-        Motor1.MotionInVelocity(200);
+        Motor1.MotionInVelocity(250);
     end
     if a < -2
         plotData = [];
-        Motor1.MotionInVelocity(-200);
+        Motor1.MotionInVelocity(-250);
+    end
+    if i > 17700
+        for timer = 1:n
+            Motor1.MotionInVelocity(-50);
+        end
+    end
+    if i < -12900
+        for timer1 = 1:m
+            Motor1.MotionInVelocity(50);
+        end
     end
 end
-
-
-%     while a > 0 & loop == 1
-%         fprintf('pls\n')
-%         loop = 0;
-%     end
-%     loop = 1;
-%     while a < 0 & loop == 1
-%         fprintf('ye\n')
-%         loop = 0;
-%     end
-%     switch a
-%         case a > 0
-%             fprintf('pls\n')
-%         case a < 0
-%             fprintf('ye\n')
-%     end
-
-    %plotData(1:end,5)
-    %signalName = shimmer.getsignalname(3)
-
-    
-% a = magic(10)
-% 
-% a(1:end,5)
-%     
     
